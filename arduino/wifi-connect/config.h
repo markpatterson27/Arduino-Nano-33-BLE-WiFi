02b0acf04f38d0654c614493bfb51d4fa2e499e3
Mark Patterson <35724907+markpatterson27@users.noreply.github.com>
2020-07-12 11:03:29 +0100

    added arduino sketch for wifi connection

---
+++ arduino/wifi-connect/config.h
@@ -0,0 +1,5 @@
+/* configuration settings for project */
+
+// WiFi access point settings
+const char* ssid     = "<SSID>";         // The SSID (name) of the Wi-Fi network you want to connect to
+const char* password = "<PASSWORD>";     // The password of the Wi-Fi network
